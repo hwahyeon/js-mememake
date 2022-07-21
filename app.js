5623274e40e1cd1707fefdb562482c3682c91042
hwahyeon <hwahyeon@knou.ac.kr>
2022-07-22 00:17:46 +0900

    Add the funciton to add images

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const colorOptions = Array.from(document.getElementsByClassName("color-option"));
-const color = document.getElementById("color")
+const fileInput = document.getElementById("file");
+const modeBtn = document.getElementById("mode-btn");
+const clearBtn = document.getElementById("clear-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const colorOptions = Array.from(
+    document.getElementsByClassName("color-option")
+    ); // HTMLCollection -> Array : Because of forEach function
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
-
+let isFilling = false;
 
 function onMove(event){
     if (isPainting){
@@ -28,7 +37,7 @@ function cancelPainting() {
 }
 
 function onLineWidthChange(event){
-    ctx.lineWidth = event.target.value
+    ctx.lineWidth = event.target.value;
 }
 
 function onColorChange(event){
@@ -38,10 +47,48 @@ function onColorChange(event){
 
 function onColorClick(event){
     //console.dir(event.target.dataset.color);
-    const colorValue = event.target.dataset.color 
+    const colorValue = event.target.dataset.color ;
     ctx.strokeStyle = colorValue;
     ctx.fillStyle = colorValue;
     color.value = colorValue;
+}
+
+function onModeClick(){
+    if (isFilling){
+        isFilling = false
+        modeBtn.innerText = "Fill"
+    } else {
+        isFilling = true
+        modeBtn.innerText = "Draw"
+    }
+}
+
+function onCanvasClick(){
+    if (isFilling){
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+function onClearClick(){
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
+
+function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        fileInput.value = null;
+    }
 }
 
 
@@ -49,10 +96,13 @@ canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting); //another way : document.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
-
-
-
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onLineWidthChange);
-color.addEventListener("change", onColorChange)
+color.addEventListener("change", onColorChange);
 
-colorOptions.forEach(color => color.addEventListener("click", onColorClick))
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+clearBtn.addEventListener("click", onClearClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
