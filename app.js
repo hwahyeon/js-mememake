8171c075bc573b2d7d6b73e7e12c58464c8a6cae
hwahyeon <hwahyeon@knou.ac.kr>
2022-07-21 20:19:03 +0900

    Update a Function of Paint Color

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const color = document.getElementById("color")
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 
@@ -21,6 +24,24 @@ function startPainting(){
 
 function cancelPainting() {
     isPainting = false;
+    ctx.beginPath(); // width range등의 적용을 받지 않기 위해 한 번 끊어줌.
+}
+
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value
+}
+
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event){
+    //console.dir(event.target.dataset.color);
+    const colorValue = event.target.dataset.color 
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
 }
 
 
@@ -28,3 +49,10 @@ canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting); //another way : document.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange)
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick))
